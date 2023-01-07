bc5aa054299e5882c985d3c9c0def811c05d942c
npmrun <kk1549469775@gmail.com>
2023-01-07 12:00:10 +0800

    收藏类型不添加rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,17 +3,19 @@ import { SITE_TITLE, SITE_DESCRIPTION, SITE_LANG } from '@blog/config';
 import { published } from '@blog/utils';
 
 const posts = await published();
+console.log(posts.map(v=>v.mode));
 /**
  * 10篇最新的文章
  */
 export const get = () => {
+  
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     stylesheet: true,
     customData: `<language>${SITE_LANG || 'en-us'}</language>`,
     site: import.meta.env.SITE,
-    items: posts.filter(v=>(v.rss === undefined || v.rss)).slice(0, 10).map((post) => ({
+    items: posts.filter(v=>(v.rss === undefined || v.rss || v.mode!=="collect")).slice(0, 10).map((post) => ({
       link: post.url,
       title: post.title,
       pubDate: post.pubDate,
