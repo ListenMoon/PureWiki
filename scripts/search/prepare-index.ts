68f093bac89a74e3c9c29d17818bce2211e2eaf0
npmrun <kk1549469775@gmail.com>
2023-01-17 15:51:24 +0800

    增加简单的搜索功能

---
+++ scripts/search/prepare-index.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import { promises as fs } from "fs";
+import FastGlob from "fast-glob";
+import grayMatter from "gray-matter";
+
+(async function () {
+    const publicDir = path.join(process.cwd(), "public");
+    const contentDir = path.join(process.cwd(), "./article");
+    const indexFile = path.join(publicDir, "search-index.json");
+    const getSlugFromPathname = (pathname) => {
+        return pathname.replace(/\.md/, '');
+    }
+
+    const contentFilePaths = await FastGlob(["**/*.md", "!drafts/**/*.md"], {
+        cwd: contentDir,
+    });
+
+    if (contentFilePaths.length) {
+        const files = contentFilePaths.map(
+            async (filePath) =>
+                await fs.readFile(
+                    path.resolve(process.cwd(), "./article", filePath),
+                    "utf8"
+                )
+        );
+        const index = [];
+        let i = 0;
+        for await (let file of files) {
+            const {
+                data: { title, description, tags },
+                content,
+            } = grayMatter(file);
+            index.push({
+                slug: getSlugFromPathname(contentFilePaths[i]),
+                category: "blog",
+                title,
+                description,
+                tags,
+                body: content,
+            });
+            i++;
+        }
+        await fs.writeFile(indexFile, JSON.stringify(index));
+        console.log(
+            `Indexed ${index.length} documents from ${contentDir} to ${indexFile}`
+        );
+    }
+})();
