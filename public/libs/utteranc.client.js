15c55e8e2076877365ecebc42f88a1c2778f2ceb
npmrun <kk1549469775@gmail.com>
2023-08-12 10:49:33 +0800

    add

--- public/libs/utteranc.client.js
+++ public/libs/utteranc.client.js
@@ -1,23 +1,26 @@
-var script = document.createElement("script")
-script.setAttribute("src", "https://utteranc.es/client.js")
-script.setAttribute("repo", "ListenMoon/PureWiki")
-script.setAttribute("issue-term", "pathname")
-script.setAttribute("label", "收录")
-script.setAttribute("theme", "github-light")
-script.setAttribute("crossorigin", "anonymous")
-script.setAttribute("async", "")
+var script = document.createElement("script");
+script.setAttribute("src", "https://utteranc.es/client.js");
+script.setAttribute("repo", "ListenMoon/PureWiki");
+script.setAttribute("issue-term", "pathname");
+script.setAttribute("label", "收录");
+script.setAttribute("theme", "github-light");
+script.setAttribute("crossorigin", "anonymous");
+script.setAttribute("async", "");
 
-script.onerror = function(){
-    if(sc){
-        sc.remove()
+script.onerror = function () {
+    if (sc) {
+        sc.remove();
     }
-}
+};
+script.onload = function () {
+    if (sc) {
+        parentNode.removeChild(sc);
+    }
+};
+var sc = document.getElementById("utteranc-sc");
+var parentNode = sc.parentNode;
+parentNode.appendChild(script);
 
-var sc = document.getElementById("utteranc-sc")
-var parentNode = sc.parentNode
-parentNode.removeChild(sc)
-parentNode.appendChild(script)
-
-if(document.currentScript && document.currentScript.parentNode){
-    document.currentScript.parentNode.removeChild(document.currentScript)
+if (document.currentScript && document.currentScript.parentNode) {
+    document.currentScript.parentNode.removeChild(document.currentScript);
 }
